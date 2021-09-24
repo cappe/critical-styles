084ba0f4830d86301a59f33b428a6679f27ecda3
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-24 17:19:31 +0300

    Let docker-compose decide host URL

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-  include ActiveStorage::SetCurrent
-  #
-  # TODO: Replace hard-coded value with something dynamic. Maybe from docker-compose.yaml?
-  # before_action do
-  #   ActiveStorage::Current.host = 'http://localhost:3000'
-  # end
+  before_action do
+    ActiveStorage::Current.host = ENV.fetch("HOST_URL")
+  end
 end
