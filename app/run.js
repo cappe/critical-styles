0bdf98c83522cbfd9be460a41b584970b0212021
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-24 14:27:05 +0300

    Adds a standalone script for minimalcss

---
+++ app/run.js
@@ -0,0 +1,22 @@
+// If we need to run minimalcss in a standalone script, use this
+//
+// const minimalcss = require('minimalcss');
+//
+// minimalcss
+//     .minimize({
+//         url: 'http://192.168.10.41:8001/fi/pay-n-play/',
+//         // whitelist: ['md*'],
+//         // debug: true,
+//         loadimages: true,
+//         withoutjavascript: false,
+//         styletags: true,
+//         // enableServiceWorkers: true,
+//     })
+//     .then(({ finalCss, stylesheetContents }) => {
+//         // console.log(Object.values(stylesheetContents));
+//         console.log(finalCss);
+//     })
+//     // .catch(error => {
+//     //     console.error(`Failed the minimize CSS: ${error}`);
+//     // });
+//
