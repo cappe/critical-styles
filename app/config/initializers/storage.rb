dce866890e56e01ac0b98c08170b0cfff1dc1999
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2021-09-25 14:53:43 +0300

    Attempts to fix issues with puppeteer not working in production

--- app/config/initializers/storage.rb
+++
@@ -1,5 +0,0 @@
-# MinimalCSS needs to be run by non-root user. Non-root user is set up
-# in Dockerfile, and it has writing access only to its home directory.
-# Hence, we have a dedicated tmp storage.
-tmp_storage = ENV.fetch("TMP_STORAGE")
-FileUtils.mkdir_p(tmp_storage) unless File.directory?(tmp_storage)
